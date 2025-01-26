3fdcb4db7e71e579b9dbc6288498073fc21629bb
Novemberman90 <alexledovit@gmail.com>
2025-01-27 00:37:30 +0200

    add db.js and add function createCard

--- js/main.js
+++ js/main.js
@@ -132,4 +132,38 @@ window.addEventListener('DOMContentLoaded', ()=>{
   });
 
 
+  // render product cards
+  const createCard = (data)=> {
+    const productItemBlock = document.querySelector('.product__item-block');
+    if (!productItemBlock) {
+      console.error('Элемент .product__item-block не найден');
+      return;
+    }
+
+    data.cards.forEach(({img, altimg, title, descr, price}) => {
+
+      if (!img || !altimg || !title || !descr || price === undefined) {
+      console.warn('Неполные данные для карточки:', { img, altimg, title, descr, price });
+      return;
+      }
+      
+      const productItem = document.createElement('div');
+      const classes = 'product__item wow animate__animated animate__zoomIn';
+      productItem.classList.add(...classes.split(' '));
+      productItem.innerHTML = `
+    
+        <h3 class="product__item-title">${title}</h3>
+          <p class="product__descr">${descr}</p>
+        <img class="product__img" src=${img} alt=${altimg}>
+        <div class="product__prise-box">
+          <span class="product__prise">${price}</span>
+          <span class="product__curency">UAH</span> / kg
+        </div>
+      
+      `;
+      productItemBlock.append(productItem);
+    });
+  }
+  createCard(data);
+
 });
